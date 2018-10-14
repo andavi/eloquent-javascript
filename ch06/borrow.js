9069da0df29248aef4be518a2e03572ab8b58bf2
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-10-14 15:16:53 -0700

    start robot

--- ch06/borrow.js
+++ ch06/borrow.js
@@ -1,5 +1,5 @@
 let map = {one: true, two: true, hasOwnProperty: true};
 
-console.log(Object.prototype.hasOwnProperty.call(map, 'one'));
 // console.log(map.hasOwnProperty("one"));
+console.log(Object.prototype.hasOwnProperty.call(map, 'one'));
 // → true
